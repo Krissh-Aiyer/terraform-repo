2883bd5882913286fab2964fa1bfa67a0b0a6efc
Krissh-Aiyer <krisshaiyer@gmail.com>
2025-07-27 16:59:08 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,6 +5,7 @@ region = “us-east-1”
 resource “aws_instance” “one” {
 ami = “copy from ami catalog”
 instance_type = “t2.micro”
+#this security_group already created & saved
 vpc_security_group_ids
 tags = {
 Name = “rahaminstance”
